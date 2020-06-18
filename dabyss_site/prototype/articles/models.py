bf8a264ff908ca85dcc6067dc0afb63f85cfa932
Ayumu-Nono <ayumu.bete@gmail.com>
2020-06-18 16:37:57 +0900

    add 2 models

--- dabyss_site/prototype/articles/models.py
+++ dabyss_site/prototype/articles/models.py
@@ -4,12 +4,31 @@ from markdownx.models import MarkdownxField
 
 class HomeArticle(models.Model):
     title = models.CharField(max_length=40)
-    summary = models.TextField(blank=False)
+    summary = models.TextField('概要', blank=False)
     text = MarkdownxField('本文', help_text='Markdown形式で書いてください。')
     date = models.DateTimeField(blank=False)
 
     def __str__(self):
         return self.title
 
+
+class BlogArticle(models.Model):
+    title = models.CharField(max_length=40)
+    summary = models.TextField('概要', blank=False)
+    text = MarkdownxField('本文', help_text='Markdown形式で書いてください。')
+    date = models.DateTimeField(blank=False)
+
+    def __str__(self):
+        return self.title
+
+
+class GamesArticle(models.Model):
+    title = models.CharField(max_length=40)
+    summary = models.TextField('概要', blank=False)
+    text = MarkdownxField('本文', help_text='Markdown形式で書いてください。')
+    date = models.DateTimeField(blank=False)
+
+    def __str__(self):
+        return self.title
 
 
