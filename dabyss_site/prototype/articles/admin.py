10733bb5c9f9905eccc62829b9142d3921b61365
Ayumu-Nono <ayumu.bete@gmail.com>
2020-06-18 16:13:36 +0900

    add "HomeArticle" to admin

--- dabyss_site/prototype/articles/admin.py
+++ dabyss_site/prototype/articles/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import HomeArticle
+from markdownx.admin import MarkdownxModelAdmin
 
-# Register your models here.
+admin.site.register(HomeArticle, MarkdownxModelAdmin)
