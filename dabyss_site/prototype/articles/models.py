5b6c3aa1850fb0e9ac2009f026077063651c0522
Ayumu-Nono <ayumu.bete@gmail.com>
2020-06-18 15:50:38 +0900

    add model "HomeArticle"

--- dabyss_site/prototype/articles/models.py
+++ dabyss_site/prototype/articles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class HomeArticle(models.Model):
+    title = models.CharField(max_length=40)
+    summary = models.TextField(blank=False)
+    text = models.TextField(blank=False)
+    date = models.DataTimeField(blank=False)
+
+    def __str__(self):
+        return self.title
+
+        
+
