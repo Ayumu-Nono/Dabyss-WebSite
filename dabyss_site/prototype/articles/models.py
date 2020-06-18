30008e01dbcb9aa5ae3ab2815d5c964e03c312ed
Ayumu-Nono <ayumu.bete@gmail.com>
2020-06-18 16:02:00 +0900

    add app "markdownx"

--- dabyss_site/prototype/articles/models.py
+++ dabyss_site/prototype/articles/models.py
@@ -4,11 +4,11 @@ from django.utils import timezone
 class HomeArticle(models.Model):
     title = models.CharField(max_length=40)
     summary = models.TextField(blank=False)
-    text = models.TextField(blank=False)
+    text = MarkdownxField('本文', help_text='Markdown形式で書いてください。')
     date = models.DataTimeField(blank=False)
 
     def __str__(self):
         return self.title
 
-        
+
 
--- dabyss_site/prototype/prototype/urls.py
+++ dabyss_site/prototype/prototype/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 from .views import TopView
 from . import settings
@@ -22,4 +22,5 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="top.html"), name="top"),
+    path('markdownx/', include('markdownx.urls')),
 ]
