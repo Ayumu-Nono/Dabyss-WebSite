6045a501adf0ef0a9ff7d2e139b19939cba54a37
Ayumu-Nono <ayumu.bete@gmail.com>
2020-06-19 10:03:13 +0900

    add media

--- dabyss_site/prototype/prototype/urls.py
+++ dabyss_site/prototype/prototype/urls.py
@@ -18,9 +18,16 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from .views import TopView
 from . import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="top.html"), name="top"),
     path('markdownx/', include('markdownx.urls')),
 ]
+
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
